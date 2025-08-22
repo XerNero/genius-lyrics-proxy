6cc6402ea9bc573e56ffead1a760e361297a3be6
Xerya <134085748+XerNero@users.noreply.github.com>
2025-08-22 23:29:33 +0700

    Create health.js

---
+++ api/health.js
@@ -0,0 +1,5 @@
+// Cek cepat apakah function jalan
+export default async function handler(_req, res) {
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send("OK");
+}
